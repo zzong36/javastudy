541f7c472f1ce01947370f8518d3ffc1267ea6e1
SEOKYUNG <lastsun4@naver.com>
2022-03-15 06:21:26 +0900

    dd

--- JAVA/src/kr/ac/kopo/day04/homework/Homework05.java
+++ JAVA/src/kr/ac/kopo/day04/homework/Homework05.java
@@ -6,22 +6,56 @@ import java.util.Scanner;
 public class Homework05 {
 
 	public static void main(String args[]) {
-
 		Scanner sc = new Scanner(System.in);
-		int[] nums = new int[] { 1, 2, 3, 4, 5 };
-		int[] copy = new int[5]; 
 
-		
-		System.out.print("int[] nums = ");
-		System.out.println(Arrays.toString(nums));
+		int[] nums = { 1, 2, 3, 4, 5 };
+		char ch = ' ';
+
+		System.out.print("int[] nums: ");
+		System.out.print(Arrays.toString(nums));
+
 		System.out.println();
-
-		System.out.println("Left(-), Right(+): ");
+		System.out.print("Left(-), Right(+): ");
 		String sign = sc.nextLine();
+		ch = sign.charAt(0);
 
-		System.out.println("Start Index(0-4): ");
-		int index = sc.nextInt(4);
+		System.out.println(ch);
 
-		System.out.println("< PRINT >");
+		System.out.print("Start Index(0-4) : ");
+		int index = sc.nextInt();
+
+		int[] copy = new int[nums.length];
+
+		if (ch == '+') {
+
+			for (int i = 0; i < nums.length; i++) {
+
+				if (i + index < 5) {
+
+					copy[i] = nums[i + index];
+				} else {
+
+					copy[i] = nums[i + index - 5];
+				}
+
+			}
+		} else if (ch == '-') {
+
+			for (int i = 0; i < nums.length; i++) {
+
+				if (i + index < 5) {
+
+					copy[i] = nums[i + index];
+				} else {
+
+					copy[i] = nums[i + index - 5];
+				}
+
+			}
+
+		}
+
+		System.out.print(Arrays.toString(copy));
+
 	}
 }
