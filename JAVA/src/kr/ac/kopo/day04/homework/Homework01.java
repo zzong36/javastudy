efbcc04fbd563024d7c17f5097b4f0ac4d00ed62
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-03-14 17:54:52 +0900

    숙제 화이팅

---
+++ JAVA/src/kr/ac/kopo/day04/homework/Homework01.java
@@ -0,0 +1,16 @@
+package kr.ac.kopo.day04.homework;
+
+import java.util.Scanner;
+
+public class Homework01 {
+	
+	public static void main(String args[]) {
+		
+		Scanner sc = new Scanner(System.in);
+
+		
+		
+		
+	}
+
+}
