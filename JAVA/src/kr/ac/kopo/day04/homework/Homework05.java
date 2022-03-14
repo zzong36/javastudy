5cff5a8b2221581750803c2269224e86aa988433
SEOKYUNG <lastsun4@naver.com>
2022-03-15 01:41:59 +0900

    c

--- JAVA/src/kr/ac/kopo/day04/homework/Homework05.java
+++ JAVA/src/kr/ac/kopo/day04/homework/Homework05.java
@@ -5,25 +5,23 @@ import java.util.Scanner;
 
 public class Homework05 {
 
-	
 	public static void main(String args[]) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		int[] nums = new int [] {1,2,3,4,5};
+		int[] nums = new int[] { 1, 2, 3, 4, 5 };
+		int[] copy = new int[5]; 
+
 		
-		System.out.print("int[] nums = " );
+		System.out.print("int[] nums = ");
 		System.out.println(Arrays.toString(nums));
 		System.out.println();
-		
+
 		System.out.println("Left(-), Right(+): ");
 		String sign = sc.nextLine();
-		
+
 		System.out.println("Start Index(0-4): ");
 		int index = sc.nextInt(4);
-		
-		System.out.println("< PRITN >");
-		
-		
-		
+
+		System.out.println("< PRINT >");
 	}
 }
