f42c9b33567de729554574021b7285e01d6db4b1
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-03-14 15:27:29 +0900

    배열 수업자료 1

---
+++ JAVA/src/kr/ac/kopo/day04/ArrayMain01.java
@@ -0,0 +1,30 @@
+package kr.ac.kopo.day04;
+
+public class ArrayMain01 {
+	
+	public static void main(String args[]) {
+		
+		int [] arr;
+		
+		// 정수 5개를 가지는 배열 생성
+		
+		int size = 5;
+		
+		arr = new int[size];
+		
+		System.out.println("arr :" + arr);
+		System.out.println("배열의 원소 총 개수 : " + arr.length + "개");
+		System.out.println("첫번째 원소: " + arr[0]);
+		System.out.println("두번째 원소: " + arr[1]);
+		System.out.println("세번째 원소: " + arr[2]);
+		System.out.println("네번째 원소: " + arr[3]);
+		System.out.println("다섯번째 원소: " + arr[4]);
+		
+		System.out.println("-----------------------------");
+		for(int i = 0; i < arr.length; i++) { // 배열 때문에 i를 쓴다
+			System.out.println(i + "번째 원소 : " + arr[i]);
+			
+		}
+	}
+
+}
