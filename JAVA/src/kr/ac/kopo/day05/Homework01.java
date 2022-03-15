048bac0df71fc0f756f534992c171e36ecbcc02b
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-03-15 17:56:21 +0900

    숙제 고민중

---
+++ JAVA/src/kr/ac/kopo/day05/Homework01.java
@@ -0,0 +1,31 @@
+package kr.ac.kopo.day05;
+
+import java.util.Scanner;
+
+public class Homework01 {
+
+	public static void main(String args[]) {
+
+
+	Scanner sc = new Scanner(System.in);
+
+	int arr[] = new int[3];
+	
+	for(int i = 0; i < 3; i++) {
+			
+			System.out.printf("*** %d번째 아이스크림 구매정보 ***%n", i+1);
+			System.out.print("아이스크림 가격: ");
+			arr[i] = sc.nextInt();
+		
+	}
+	
+	System.out.println("총 ");
+
+		
+	
+	
+	
+
+		
+	}
+}
---
+++ JAVA/src/kr/ac/kopo/day05/Icecream.java
@@ -0,0 +1,8 @@
+package kr.ac.kopo.day05;
+
+public class Icecream {
+	
+	String name; //아이스크림명
+	int price;   //아이스크림 가격
+
+}
