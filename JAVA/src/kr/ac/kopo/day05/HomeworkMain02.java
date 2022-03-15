b4fcff721f562b7739dbe8cc97f684e5ded81bf8
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-03-15 10:40:15 +0900

    숙제리뷰

--- JAVA/src/kr/ac/kopo/day05/HomeworkMain02.java
+++ JAVA/src/kr/ac/kopo/day05/HomeworkMain02.java
@@ -8,32 +8,45 @@ public class HomeworkMain02 {
 
 		int[] nums = new int[5];
 		Scanner sc = new Scanner(System.in);
-		
+
 		String msg = "";
-		for(int i = 0; i < nums.length;) {
-			System.out.print(i+1 + "번째 정수 입력 : ");
+		for (int i = 0; i < nums.length;) {
+			System.out.print(i + 1 + "번째 정수 입력 : ");
 			nums[i] = sc.nextInt();
-			if(i > 0 && nums[i] < nums[i-1]) {
+			if (i > 0 && nums[i] < nums[i - 1]) {
 				System.out.println(msg + "보다 큰수여야합니다");
 			} else {
 				msg += nums[i++] + " ";
 			}
 		}
-		
-		/*
-		for(int i = 0; i < nums.length; i++) {
-			System.out.print(i+1 + "번째 정수 입력 : ");
-			nums[i] = sc.nextInt();
-			if(i > 0 && nums[i] < nums[i-1]) {
-				for(int j = 0; j < i; j++) {
-					System.out.print(nums[j] + " ");
-				}
-				System.out.println("보다 큰수여야합니다");
-				i--;
-			}
-		}
-		*/
 
+		/*
+		 * for(int i = 0; i < nums.length; i++) { 
+		 * System.out.print(i+1 + "번째 정수 입력 : ");
+		 * nums[i] = sc.nextInt(); 
+		 * if(i > 0 && nums[i] < nums[i-1]) { 
+		 * for(int j = 0; j <i; j++) { 
+		 * System.out.print(nums[j] + " "); }
+		 * System.out.println("보다 큰수여야합니다"); i--; } 
+		 * }
+		 */
+
+		System.out.println("< PRINT >");
+		for (int num : nums) {
+			System.out.print(num + "\t");
+		}
+		System.out.println();
+
+		System.out.println("< REVERSE >");
+		for (int i = nums.length - 1; i >= 0; i--) {
+			// System.out.print(nums[i] + " ");
+			
+			int n10 = nums[i] /10;
+			int n1 = nums[i] % 10;
+			
+			System.out.print(n1*10 + n10 + " ");
+		}
+		System.out.println();
 	}
 
 }
