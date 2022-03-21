67f910ea403b145721b496db47836fbf00f5b371
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-03-21 09:33:19 +0900

    f

--- JAVA/src/kr/ac/kopo/day07/Member.java
+++ JAVA/src/kr/ac/kopo/day07/Member.java
@@ -2,52 +2,111 @@ package kr.ac.kopo.day07;
 
 public class Member {
 	
-	String name; 
-	int age;
-	String  bloodType;
-	/*	String name = 알 수 없음; 이렇게 서도 됨
-		int age;
-		String  bloodType;
-	*/
-	
+	String name;
+	int    age;
+	String bloodType;
 	
 	Member() {
-		this("알 수 없음", -1, "알 수 없음");
+		name = "알수없음";
+		age = -1;
+		bloodType = "알수없음";
 	}
 	
 	Member(String name) {
-		this(name, -1, "알 수 없음");
-		
+		this.name = name;
+		this.age = -1;
+		this.bloodType = "알수없음";
 	}
 	
 	Member(String name, int age) {
-		this(name, age, "알 수 없음");
-				
+		this.name = name;
+		this.age = age;
+		this.bloodType = "알수없음";
 	}
-	/*
-	 
-	Member(String n, int a, String bt) {
-	 	this. name = n; // this를 써주는게 더 일반적이다. 
-		age = a;
-		bloodType = bt;
-	}
-	 */
-	
 	
 	Member(String name, int age, String bloodType) {
-		this.name = name; // 지역변수와 매개변수를 ㅜ부하기 ㅟ해
+		this.name = name;
 		this.age = age;
 		this.bloodType = bloodType;
 	}
 	
+	void info() {
+		System.out.println("이름 : " + name 
+				+ ", 나이 : " + age
+				+ ", 혈액형 : " + bloodType);
+	}
 	
+}
+
+/*
+public class Member {
+
+	String name;
+	int    age;
+	String bloodType;
 	
+	Member() {
+		this("알수없음", -1, "알수없음");
+	}
 	
+	Member(String name) {
+		this(name, -1, "알수없음");
+	}
 	
+	Member(String name, int age) {
+		this(name, age, "알수없음");
+	}
+	
+	Member(String name, int age, String bloodType) {
+		this.name = name;
+		this.age = age;
+		this.bloodType = bloodType;
+	}
 	
 	void info() {
-		System.out.println("이름 : " + name + " 나이: " + age + " 혈액형 : " + bloodType); // 자신의 인스턴스 객체가 heap에서 사라질 때가지 값을 저장함
-
+		System.out.println("이름 : " + name 
+				+ ", 나이 : " + age
+				+ ", 혈액형 : " + bloodType);
 	}
-
+	
 }
+*/
+/*
+public class Member {
+
+	String name;
+	int    age;
+	String bloodType;
+	
+	Member() {
+		name = "알수없음";
+		age = -1;
+		bloodType = "알수없음";
+	}
+	
+	Member(String name) {
+		this.name = name;
+		this.age = -1;
+		this.bloodType = "알수없음";
+	}
+	
+	Member(String name, int age) {
+		this.name = name;
+		this.age = age;
+		this.bloodType = "알수없음";
+	}
+	
+	Member(String name, int age, String bloodType) {
+		this.name = name;
+		this.age = age;
+		this.bloodType = bloodType;
+	}
+	
+	void info() {
+		System.out.println("이름 : " + name 
+				+ ", 나이 : " + age
+				+ ", 혈액형 : " + bloodType);
+	}
+	
+}
+*/
