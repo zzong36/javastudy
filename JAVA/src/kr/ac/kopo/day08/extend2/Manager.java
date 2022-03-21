215de4c5c4607a6560ad9b390bf420a69e64028c
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-03-21 16:08:08 +0900

    ㄹㄹ

--- JAVA/src/kr/ac/kopo/day08/extend2/Manager.java
+++ JAVA/src/kr/ac/kopo/day08/extend2/Manager.java
@@ -1,5 +1,17 @@
 package kr.ac.kopo.day08.extend2;
 
 public class Manager extends Employee {
+	
+	Employee [] emplist;
+
+	public Manager() {
+	}
+
+	public Manager(String name, int salary, String grade, Employee[] emplist) {
+		super(name, salary, grade);
+		this.emplist = emplist;
+	}
+
+	
 
 }
