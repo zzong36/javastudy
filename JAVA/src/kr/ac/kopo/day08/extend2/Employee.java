404fd2d8432bcb742b4f6ba284c6482e2e6bdcfc
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-03-21 16:08:01 +0900

    뀨륵

---
+++ JAVA/src/kr/ac/kopo/day08/extend2/Employee.java
@@ -0,0 +1,24 @@
+package kr.ac.kopo.day08.extend2;
+
+public class Employee {
+	
+	String name;
+	int salary;
+	String grade;
+
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Employee(String name, int salary, String grade) {
+		super();
+		this.name = name;
+		this.salary = salary;
+		this.grade = grade;
+	}
+
+	
+	
+
+}
---
+++ JAVA/src/kr/ac/kopo/day08/extend2/Manager.java
@@ -0,0 +1,5 @@
+package kr.ac.kopo.day08.extend2;
+
+public class Manager extends Employee {
+
+}
