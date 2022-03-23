452afb3e305355aa6713b4f08f4edba2fd502a7c
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-03-23 09:26:29 +0900

    ff

--- JAVA/src/kr/ac/kopo/day09/CastMain01.java
+++ JAVA/src/kr/ac/kopo/day09/CastMain01.java
@@ -2,7 +2,63 @@ package kr.ac.kopo.day09;
 
 public class CastMain01 {
 	
+	/*
+	public static void print(Child01 c) {
+		c.print();
+	}
+	
+	public static void print(Child02 c) {
+		c.print();
+	}
+	*/
+	public static void print(Parent p) {
+		p.print();
+		
+		if(p instanceof Child01) {
+			Child01 c = (Child01)p;
+			c.study();
+			c.sleep();
+		} else if(p instanceof Child02) {
+			Child02 c = (Child02)p;
+			c.sing();
+			c.play();
+		} else {
+			
+		}
+		
+		//System.out.println("Child01 : "+ (p instanceof Child01)); // 상속 관계에서만 사용 가능
+		//System.out.println("Child01 : "+ (p instanceof Child02)); 
+		//System.out.println("Child01 : "+ (p instanceof Parent)); 
+		
+	}
+	
 	public static void main(String[] args) {
+
+		// print(new Parent());
+		// print(new Child01());
+		//print(new Child02());
+		
+		
+		
+		
+		
+		/*
+		 * 
+		Child01 c01 = new Child01();
+		print(c01);
+		
+//		Child01 c01 = new Child01();
+//		c01.print();
+		
+		Child02 c02 = new Child02();
+		print(c02);
+//		c02.print();
+		
+		Parent p = new Parent();
+		print(p);
+//		p.print();
+		
+		 */
 		
 	}
 
---
+++ JAVA/src/kr/ac/kopo/day09/CastMain02.java
@@ -0,0 +1,16 @@
+package kr.ac.kopo.day09;
+
+public class CastMain02 {
+
+	public static void main(String args[]) {
+		
+		// Child01 c = (Child01) new Parent();
+		
+		Parent p = new Child01();
+		Child01 c2 = (Child01)p;
+		
+		c2.sleep();
+		c2.study();
+	}
+	
+}
