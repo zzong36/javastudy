34919c4f9115314ce91f9fff520a69ae218c7077
zzong36 <100885809+zzong36@users.noreply.github.com>
2022-03-24 17:41:40 +0900

    dd

--- JAVA/src/kr/ac/kopo/day11/ListMain.java
+++ JAVA/src/kr/ac/kopo/day11/ListMain.java
@@ -18,29 +18,43 @@ import java.util.List;
  */
 
 public class ListMain {
-	
+
 	public static void main(String args[]) {
-		
+
 //		List<String> list = new ArrayList<String>();
 //		List<String> list = new ArrayList<>();	// 생략가능
-		List<String> list = new LinkedList<>();	// Linked로 해도 문제없이 돌아감
+		List<String> list = new LinkedList<>(); // Linked로 해도 문제없이 돌아감
 		list.add("one");
 		list.add("two");
 		list.add("three");
 		list.add("four");
-		
-		System.out.println("전체 원소의 개수 : " + list.size()+ "개");
-		
+		list.add("one"); // 중복허용
+
+		System.out.println("전체 원소의 개수 : " + list.size() + "개");
+
 		System.out.println("< PRINT >");
-		for(int i = 0; i < list.size(); i++) {
+		for (int i = 0; i < list.size(); i++) {
 			System.out.println(list.get(i));
 		}
-		
+
+		System.out.println("\"one\" 삭제 :" + (list.remove("one") ? "성공" : "실패"));
+		System.out.println("\"one\" 삭제 :" + (list.remove("one") ? "성공" : "실패")); // 0번지에 위치한건 two가 됨
+		System.out.println("\"one\" 삭제 :" + (list.remove("one") ? "성공" : "실패")); 
+		System.out.println();
 		System.out.println("<1.5 버전 >");
-		for(String str : list) {
+		for (String str : list) {
 			System.out.println(str);
 		}
 		
+		System.out.println("0번지 문자열 : " + list.get(0));
+		System.out.println("0번지에 위치한 문자열 삭제 : " + list.remove(0));
+
+		System.out.println();
+		
+		System.out.println("<1.5 버전 >");
+		for (String str : list) {
+			System.out.println(str);
+		}
 	}
 
 }
